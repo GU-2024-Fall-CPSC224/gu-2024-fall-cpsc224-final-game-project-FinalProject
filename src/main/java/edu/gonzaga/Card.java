54b9f4891588893a467020d0764491a012bcb890
Mia Procel <miaprocel77@gmail.com>
2024-11-02 10:44:41 -0700

    Card class.

--- src/main/java/edu/gonzaga/Card.java
+++ src/main/java/edu/gonzaga/Card.java
@@ -3,10 +3,11 @@ package edu.gonzaga;
 public class Card {
     String square1;
     String square2;
-    
-    public Card(String square1, String square2){
+    boolean specialCard;
+    public Card(String square1, String square2, boolean specialCard){
         this.square1 = square1;
         this.square2 = square2;
+        this.specialCard = specialCard;
         
     }
    public String getSquare1(){
