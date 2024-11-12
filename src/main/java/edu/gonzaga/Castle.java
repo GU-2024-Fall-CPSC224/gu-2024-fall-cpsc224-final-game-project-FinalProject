5ffee19cfc00f3f7cbc16411e1258f697ef15627
abbyfewel <akfewel@outlook.com>
2024-11-12 15:37:22 -0800

    added getters and setters to Player

---
+++ src/main/java/edu/gonzaga/Castle.java
@@ -0,0 +1,11 @@
+public class Castle {
+    private int height;
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
