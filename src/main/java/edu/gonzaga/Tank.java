ad9d7b18b4a6c5e1f9d371030482af3aeeb4ef4a
ADH-17 <ayden.humphries17@gmail.com>
2024-11-12 15:51:58 -0800

    Getters and Setters are all done for Tank Class, Tests are all made as well and they all pass successfully

--- src/main/java/edu/gonzaga/Tank.java
+++ src/main/java/edu/gonzaga/Tank.java
@@ -1,23 +1,52 @@
 package edu.gonzaga;
-import java.util.ArrayList;
-import java.util.Scanner;
 /*
- * This class holds all the code under main. This is where the stuff under the abstraction happens.
+ * This is Tank class holds all information regarding Tanks the players will be controlling. 
  */
 
 public class Tank { 
+    private Integer health; 
     private Integer xCord;
     private Integer yCord;
     private String color;
 
     public Tank() {
+        health = 100;
         xCord = 0;
-        yCord = 0;
-        color = "Red";
+        yCord = 0; 
+        color = "Red"; 
     }
 
     public String getColor() {
-        return color;
+        return this.color;
+    }
+
+    public Integer getHealth(){
+        return this.health; 
+    }
+
+    public Integer getXCord(){
+        return this.xCord;
+    }
+
+    public Integer getYCord(){
+        return this.yCord;
+    }
+
+    public void setHealth(Integer healthSet){
+        health = healthSet; 
+
+    }
+
+    public void setXCord(Integer xSet){
+        xCord = xSet;
+    }
+
+    public void setYCord(Integer ySet){
+        yCord = ySet;
+    }
+
+    public void setColor(String colorSet){
+        color = colorSet; 
     }
 
 }
--- src/test/java/edu/gonzaga/TestTanks.java
+++ src/test/java/edu/gonzaga/TestTanks.java
@@ -1,15 +1,59 @@
 package edu.gonzaga;
 
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import org.junit.jupiter.api.Test;
-
 import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-class TestsTank {    
+class TestTanks {    
     @Test
     void testTankColor() {
         Tank t = new Tank();
         String expected = "Red";
         Assertions.assertEquals(expected, t.getColor());
+    }
+
+    @Test
+    void testTankHealth(){
+        Tank t = new Tank();
+        Integer expectedHealth = 100;
+        assertEquals(expectedHealth, t.getHealth());
+    }
+
+    @Test
+    void testTankXCordAndYCord(){
+        Tank t = new Tank();
+        Integer expectedXCord = 0;
+        Integer expectedYCord = 0;
+        assertEquals(expectedXCord, t.getXCord());
+        assertEquals(expectedYCord, t.getYCord());
+
+    }
+
+    @Test
+    void testSetTankHealth(){
+        Tank t = new Tank();
+        t.setHealth(200); 
+        Integer expectedHealth = 200; 
+        assertEquals(expectedHealth, t.getHealth());
+    }
+
+    @Test
+    void testSetXAndYCord(){
+        Tank t = new Tank();
+        t.setXCord(4);
+        t.setYCord(5); 
+        Integer expectedY = 5;
+        Integer expectedX = 4; 
+
+        assertEquals(expectedX, t.getXCord());
+        assertEquals(expectedY, t.getYCord());
+    }
+
+    @Test
+    void testSetColor(){
+        Tank t = new Tank();
+        t.setColor("Green"); 
+        String expectedColor = "Green"; 
+
+        assertEquals(expectedColor, t.getColor());
     }
 }
