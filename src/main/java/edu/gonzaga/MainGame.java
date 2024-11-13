d6b1e8fd27b264364915e964051ba9c2069a5733
ChristianCodez <164819049+ChristianCodez@users.noreply.github.com>
2024-11-13 11:38:00 -0800

    Made a header for the game. (#5)
    Co-authored-by: Christian Carrington <christiancarrington1379@gmail.com>

--- src/main/java/edu/gonzaga/MainGame.java
+++ src/main/java/edu/gonzaga/MainGame.java
@@ -12,13 +12,24 @@
  */
 package edu.gonzaga;
 
-
 /** Main program class for launching your team's program. */
 public class MainGame {
     public static void main(String[] args) {
-        System.out.println("TANKS");
-
+        System.out.println("");
+        header();
 
         // Your code here. Good luck!
+    }
+
+    public static void header() {
+        System.out.println("*****************************************************");
+        // System.out.println("****************************************************");
+        System.out.println("*  _________    ___                          ___    *");
+        System.out.println("*      |       /   \\    |\\      |   |  /    /       *");
+        System.out.println("*      |      |_____|   |  \\    |   | /     \\__     *");
+        System.out.println("*      |      |     |   |    \\  |   | \\        \\    *");
+        System.out.println("*      |      |     |   |      \\|   |  \\    ___/    *");
+        System.out.println("*****************************************************");
+        // System.out.println("****************************************************");
     }
 }
