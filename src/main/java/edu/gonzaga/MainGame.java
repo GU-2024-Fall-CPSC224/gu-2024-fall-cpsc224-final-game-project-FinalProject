12a137c9f4f2a1c93281cefdd08fa613db9d8eb0
Abby Fewel <155482626+abbyfewel@users.noreply.github.com>
2024-11-16 15:25:40 -0800

    Abby branch (#6)
    * committing castle again to  retry, using vscode now
    
    * added getters and setters to Player
    
    * tested getter method and setter method
    
    * started making a GUI

--- src/main/java/edu/gonzaga/MainGame.java
+++ src/main/java/edu/gonzaga/MainGame.java
@@ -12,13 +12,94 @@
  */
 package edu.gonzaga;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 /** Main program class for launching your team's program. */
 public class MainGame {
+    private JFrame frame = new JFrame(); // the frame that opens when the program is run
+    // panels in the border layout: 
+    private JPanel northPanel = new JPanel();
+    private JPanel centerPanel = new JPanel();
+    private JPanel southPanel = new JPanel();
+    private JPanel centerNorthPanel = new JPanel();
+    private JPanel centerSouthPanel = new JPanel();
+    // the label that goes at the bottom: 
+    private JLabel names = new JLabel("By Christain Carrington, Abby Fewel, and Ayden Humphries");
+    // buttons that go in the middle, in panel 5:
+    private JButton start = new JButton("Start");
+    private JButton howToPlay = new JButton("How to Play");
+    // image that also goes in the middle, in panel 4:
+    private JLabel tankPicture = new JLabel();
+    private JFileChooser tankImage = new JFileChooser();
+    private JFileChooser instructionsFile = new JFileChooser(); // opens when "How to Play" is clicked
+    private JFrame instructionsFrame = new JFrame();
+    private JLabel instructionsLabel = new JLabel("label");
+
     public static void main(String[] args) {
+        MainGame game = new MainGame();
         System.out.println("");
         header();
+        game.formatting();
+    }
+        
+    public void formatting() {
+        setUpButtonListeners(); // to make Start and How to Play buttons listen
 
-        // Your code here. Good luck!
+        //formats the frame:
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // defaults settings
+        frame.setSize(500, 500);
+        frame.setLayout(new BorderLayout());
+        frame.setVisible(true);
+        northPanel.setBackground(Color.GREEN); // colors 
+        centerPanel.setBackground(Color.lightGray);
+        southPanel.setBackground(Color.white);
+        northPanel.setPreferredSize(new Dimension(100, 100)); // dimensions of panels
+        centerPanel.setPreferredSize(new Dimension(100, 100));
+        southPanel.setPreferredSize(new Dimension(100, 100));
+        frame.add(northPanel,BorderLayout.NORTH);
+        frame.add(centerPanel,BorderLayout.CENTER);
+        frame.add(southPanel,BorderLayout.SOUTH);
+        southPanel.add(names);
+        centerPanel.add(centerNorthPanel,BorderLayout.NORTH); // putting border layout in center panel
+        centerPanel.add(centerSouthPanel,BorderLayout.SOUTH);
+        ImageIcon tankStartScreenIcon = new ImageIcon("TankIntroScreen.png");
+        tankPicture.setIcon(tankStartScreenIcon);
+        centerNorthPanel.add(tankPicture);
+        centerSouthPanel.add(start);
+        centerSouthPanel.add(howToPlay);
+    }
+
+    public void setUpButtonListeners() {
+        ActionListener buttonListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                if (actionEvent.getSource() == howToPlay) {
+                    instructionsFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    instructionsFrame.setSize(200, 200);
+                    instructionsFrame.setLayout(null);
+                    instructionsFrame.setVisible(true);
+                } else if (actionEvent.getSource() == start) {
+                    // will start the game
+                }
+            }
+        };
+        start.addActionListener(buttonListener);
+        howToPlay.addActionListener(buttonListener);
+    }
+
+    public void howToPlayFrame() {
+        
     }
 
     public static void header() {
