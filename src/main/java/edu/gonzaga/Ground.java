a877159d73f0fe574daeb8151b97edf5480c36af
ADH-17 <ayden.humphries17@gmail.com>
2024-12-04 22:09:43 -0800

    Added ground, only have basic necessities to make it not give errors, brain too fried rn.

---
+++ src/main/java/edu/gonzaga/Ground.java
@@ -0,0 +1,10 @@
+package edu.gonzaga;
+import org.dyn4j.geometry.Rectangle;
+
+public class Ground extends Rectangle {
+
+    public Ground(double width, double height){
+        super(width, height); 
+    }
+
+}
